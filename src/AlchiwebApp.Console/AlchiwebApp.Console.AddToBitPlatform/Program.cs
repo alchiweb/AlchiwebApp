f1d136ec5b032953f956313d39c72d00ef5c0319
alchiweb <rev@ik.me>
2026-05-27 19:32:43 +0200

    feat: add errors management + replacements of new port numbers and new user secrets

--- src/AlchiwebApp.Console/AlchiwebApp.Console.AddToBitPlatform/Program.cs
+++ src/AlchiwebApp.Console/AlchiwebApp.Console.AddToBitPlatform/Program.cs
@@ -1,80 +1,249 @@
-﻿using AlchiwebApp.Console.Core.Services;
+﻿using System.Xml.Linq;
+using AlchiwebApp.Console.Core.Services;
+using Rejigs;
 
 internal class Program
 {
-    static string _projectFolder = "";
+    static string _bitPlatformProjectFolder = "";
+    static string _sourceProjectFolder = "";
     static string _projectName = "";
     static readonly FileSearchService _searchService = new();
-    static async Task Main(string[] args)
+    static List<string> _errors = [];
+    static int[]? _bitPlatformForwardPorts;
+    static int[]? _sourceForwardPorts;
+    static string _bitPlatformUserSecrets = "";
+    static string _sourceUserSecrets = "";
+
+    private static async Task Main(string[] args)
     {
+        _bitPlatformProjectFolder = Directory.GetCurrentDirectory();
+        _projectName = Path.GetFileName(_bitPlatformProjectFolder);
+        _sourceProjectFolder = Path.Combine(_bitPlatformProjectFolder, @"..\..");
         Console.WriteLine($"AlchiwebApp / Add To BitPlatform");
-        _projectFolder = Directory.GetCurrentDirectory();
-        _projectName = Path.GetFileName(_projectFolder);
-        await RenameServerSharedProject();
-        await RenameSharedProject();
-        Console.WriteLine($"Project name: {_projectName}");
-        Console.WriteLine($"Project directory: {_projectFolder}");
+        Console.WriteLine($"Project name: {_projectName} / Project directory: {_bitPlatformProjectFolder}");
+
+        _sourceForwardPorts = ReadForwardPorts(_sourceProjectFolder);
+        _bitPlatformForwardPorts = ReadForwardPorts(_bitPlatformProjectFolder);
+        _sourceUserSecrets = ReadUserSecrets(_sourceProjectFolder);
+        _bitPlatformUserSecrets = ReadUserSecrets(_bitPlatformProjectFolder);
+
+        if (_errors.Count > 0)
+        {
+            _errors.Add($@"Aborted: critical error.");
+        }
+        else
+        {
+            try
+            {
+                await RenameServerSharedProjectAsync(true);
+                await RenameSharedProjectAsync(true);
+
+                await ReplaceForwardedPortsAndUserSecretsAsync();
+
+            }
+            catch (Exception ex)
+            {
+                _errors.Add($@"Aborted: critical error ({ex.Message}).");
+            }
+        }
+        if (_errors.Count > 0)
+        {
+            Console.WriteLine($"Done with errors/warnings:");
+            foreach (var error in _errors)
+            {
+                Console.WriteLine($" - Error: {error}");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Done without error.");
+        }
     }
-    static async Task RenameServerSharedProject()
-    {
-        try
-        {
-            File.Move($@"{_projectFolder}\src\Server\{_projectName}.Server.Shared\{_projectName}.Server.Shared.csproj", $@"{_projectFolder}\src\Server\{_projectName}.Server.Shared\{_projectName}.Server.Core.csproj");
-        }
-        catch (Exception) { }
-        try
-        {
-            Directory.Move($@"{_projectFolder}\src\Server\{_projectName}.Server.Shared", $@"{_projectFolder}\src\Server\{_projectName}.Server.Core");
-        }
-        catch (Exception) { }
 
-        var searchPattern = $@"{_projectName}.Server.Shared";
-        var listFiles = await _searchService.SearchFilesAsync([_projectFolder], ["*.*"], true, searchPattern);
-        await _searchService.ReplaceInFilesAsync(searchPattern, $@"{_projectName}.Server.Core", listFiles);
+    private static async Task ReplaceForwardedPortsAndUserSecretsAsync()
+    {
+        string launchSettingsFilename = @"launchSettings.json";
+        string csprojFilename = $@"{_projectName}.Server.*.csproj";
+
+        if (!string.Equals(_sourceUserSecrets, _bitPlatformUserSecrets, StringComparison.OrdinalIgnoreCase))
+        {
+            await ReplaceTextAsync(_bitPlatformUserSecrets, _sourceUserSecrets, false, true, [Path.Combine(_bitPlatformProjectFolder, $@"src")], [launchSettingsFilename, csprojFilename], expectedReplacements: 5);
+        }
+        if (_sourceForwardPorts?.Length != 6 || _bitPlatformForwardPorts?.Length != 6)
+        {
+            _errors.Add($@"Forward ports error.");
+            return;
+        }
+
+        int[][] expectedReplacements = [
+            [1, 1, 0, 0, 1 ],
+            [1, 0, 0, 0, 1 ],
+            [1, 0, 0, 0, 1 ],
+            [1, 0, 0, 0, 1 ],
+            [0, 0, 0, 0, 0 ],
+            [1, 0, 1, 1, 6 ]
+            ];
+        for (int i = 0; i < 6; i++)
+        {
+            if (_sourceForwardPorts[i] != _bitPlatformForwardPorts[i])
+            {
+                await ReplaceTextAsync(_bitPlatformForwardPorts[i].ToString(), _sourceForwardPorts[i].ToString(), false, true,
+                    [Path.Combine(_bitPlatformProjectFolder, $@".devcontainer")], ["devcontainer.json"],
+                    expectedReplacements: expectedReplacements[i][0]);
+                await ReplaceTextAsync(_bitPlatformForwardPorts[i].ToString(), _sourceForwardPorts[i].ToString(), false, true,
+                    [Path.Combine(_bitPlatformProjectFolder, $@".docs")], ["*.md"],
+                    expectedReplacements: expectedReplacements[i][1]);
+                await ReplaceTextAsync(_bitPlatformForwardPorts[i].ToString(), _sourceForwardPorts[i].ToString(), false, true,
+                    [Path.Combine(_bitPlatformProjectFolder, $@"src\Server\{_projectName}.Server.AppHost")], ["Program.cs"],
+                    expectedReplacements: expectedReplacements[i][2]);
+                await ReplaceTextAsync(_bitPlatformForwardPorts[i].ToString(), _sourceForwardPorts[i].ToString(), false, true,
+                    [Path.Combine(_bitPlatformProjectFolder, $@"src\Client\{_projectName}.Client.Core")], ["appsettings.json"],
+                    expectedReplacements: expectedReplacements[i][3]);
+
+                await ReplaceTextAsync(_bitPlatformForwardPorts[i].ToString(), _sourceForwardPorts[i].ToString(), false, true,
+                    [Path.Combine(_bitPlatformProjectFolder, $@"src")], [launchSettingsFilename],
+                    expectedReplacements: expectedReplacements[i][4]);
+            }
+        }
     }
-    static async Task RenameSharedProject()
+
+    private static string ReadUserSecrets(string projectPath)
     {
+        string? userSecretValue = null;
+        string userSecretsFilename = $@"src\Server\{_projectName}.Server.AppHost\{_projectName}.Server.AppHost.csproj";
         try
         {
-            File.Move($@"{_projectFolder}\src\Shared\{_projectName}.Shared.csproj", $@"{_projectFolder}\src\Shared\{_projectName}.Core.csproj");
+            var xdoc = XDocument.Load(Path.Combine(projectPath, userSecretsFilename));
+            userSecretValue = xdoc.Document?.Element("Project")?.Elements("PropertyGroup")?.Select(el => el.Element("UserSecretsId")?.Value).Where(val => val != null).FirstOrDefault();
         }
-        catch (Exception) { }
+        catch(Exception)
+        {
+            _errors.Add($@"Error reading the ""{userSecretsFilename}"" project file.");
+        }
+        if (string.IsNullOrWhiteSpace(userSecretValue))
+        {
+            _errors.Add($@"The UserSecretsId value is missing in the ""{_projectName}.Server.AppHost"" project file.");
+            userSecretValue = "";
+        }
+        return userSecretValue;
+    }
+    private static int[]? ReadForwardPorts(string projectPath)
+    {
+        int[]? forwardPorts = null;
+        string devContainerFilename = @".devcontainer\devcontainer.json";
         try
         {
-            Directory.Move($@"{_projectFolder}\src\Shared", $@"{_projectFolder}\src\{_projectName}.Core");
+            string devContainerJson = File.ReadAllText(Path.Combine(projectPath, devContainerFilename));
+            var jsonDocument = JsonSerializer.Deserialize<JsonDocument>(devContainerJson);
+            forwardPorts = jsonDocument?.RootElement.GetProperty("forwardPorts").EnumerateArray().Select(elem => elem.GetInt32()).ToArray();
+            if (forwardPorts == null || forwardPorts.Length != 6)
+            {
+                _errors.Add($@"The ""forwardPorts"" has not 6 ports in the JSON file ""{devContainerFilename}"".");
+            }
         }
-        catch (Exception) { }
+        catch (Exception)
+        {
+            _errors.Add($@"Error in the JSON file ""{devContainerFilename}"".");
+        }
+        return forwardPorts;
+    }
 
-        var searchPattern = $@"Shared/{_projectName}.Shared";
-        var listFiles = await _searchService.SearchFilesAsync([_projectFolder], ["*.*"], true, searchPattern);
-        await _searchService.ReplaceInFilesAsync(searchPattern, $@"{_projectName}.Core/{_projectName}.Core", listFiles);
+    private static async Task RenameServerSharedProjectAsync(bool matchCase = false)
+    {
+        if (!RenameDirectory($@"src\Server\{_projectName}.Server.Shared", $@"src\Server\{_projectName}.Server.Core") ||
+            !RenameFile($@"src\Server\{_projectName}.Server.Core\{_projectName}.Server.Shared.csproj", $@"src\Server\{_projectName}.Server.Core\{_projectName}.Server.Core.csproj")
+            )
+        {
+            _errors.Add($@"Aborted: the project ""{_projectName}.Server.Shared"" must exist.");
+            return;
+        }
 
-        searchPattern = searchPattern.Replace("/", @"\");
-        listFiles = await _searchService.SearchFilesAsync([_projectFolder], ["*.*"], true, searchPattern);
-        await _searchService.ReplaceInFilesAsync(searchPattern, $@"{_projectName}.Core\{_projectName}.Core", listFiles);
+        await ReplaceTextAsync($@"{_projectName}.Server.Shared", $@"{_projectName}.Server.Core", matchCase, expectedReplacements: 102);
+    }
+    private static async Task RenameSharedProjectAsync(bool matchCase = false)
+    {
 
-        searchPattern = searchPattern.Replace(@"\", @"\\");
-        listFiles = await _searchService.SearchFilesAsync([_projectFolder], ["*.*"], true, searchPattern);
-        await _searchService.ReplaceInFilesAsync(searchPattern, $@"{_projectName}.Core\\{_projectName}.Core", listFiles);
+        if (!RenameDirectory($@"src\Shared", $@"src\{_projectName}.Core") ||
+            !RenameFile($@"src\{_projectName}.Core\{_projectName}.Shared.csproj", $@"src\{_projectName}.Core\{_projectName}.Core.csproj")
+            )
+        {
+            _errors.Add($@"Aborted: the project ""{_projectName}.Shared"" must exist.");
+            return;
+        }
 
-        searchPattern = searchPattern.Replace(@"Shared\\", @"");
-        listFiles = await _searchService.SearchFilesAsync([_projectFolder], ["*.*"], true, searchPattern);
-        await _searchService.ReplaceInFilesAsync(searchPattern, $@"{_projectName}.Core", listFiles);
+        await ReplaceTextAsync($@"Shared/{_projectName}.Shared", $@"{_projectName}.Core/{_projectName}.Core", matchCase, expectedReplacements: 20);
+        await ReplaceTextAsync($@"Shared\{_projectName}.Shared", $@"{_projectName}.Core\{_projectName}.Core", matchCase, expectedReplacements: 3);
+        await ReplaceTextAsync($@"Shared\\{_projectName}.Shared", $@"{_projectName}.Core\\{_projectName}.Core", matchCase, expectedReplacements: 56);
 
-        searchPattern = $@"Shared/";
-        listFiles = await _searchService.SearchFilesAsync([_projectFolder], ["*.*"], true, searchPattern);
-        await _searchService.ReplaceInFilesAsync(searchPattern, $@"{_projectName}.Core/", listFiles);
+        await ReplaceTextAsync($@"{_projectName}.Shared", $@"{_projectName}.Core", matchCase, expectedReplacements: 339);
 
-        searchPattern = $@"Shared`";
-        listFiles = await _searchService.SearchFilesAsync([_projectFolder], ["*.*"], true, searchPattern);
-        await _searchService.ReplaceInFilesAsync(searchPattern, $@"{_projectName}.Core`", listFiles);
+        await ReplaceTextAsync($@"Shared/", $@"{_projectName}.Core/", matchCase, expectedReplacements: 54);
+        await ReplaceTextAsync($@"Shared`", $@"{_projectName}.Core`", matchCase, expectedReplacements: 3);
+        await ReplaceTextAsync($@"Shared""", $@"{_projectName}.Core""", matchCase, expectedReplacements: 1);
 
-        searchPattern = $@"Shared""";
-        listFiles = await _searchService.SearchFilesAsync([_projectFolder], ["*.*"], true, searchPattern);
-        await _searchService.ReplaceInFilesAsync(searchPattern, $@"{_projectName}.Core""", listFiles);
+        await ReplaceTextAsync($@"Shared project to", $@"shared project (`{_projectName}.Core`) to", matchCase, expectedReplacements: 1);
+    }
+    private static async Task<int> ReplaceTextAsync(string searchText, string replaceText, bool matchCase = false, bool matchWholeWord = false, string[]? directories = null, string[]? filters = null, int? expectedReplacements = null)
+    {
+        if (directories == null || directories.Length == 0)
+        {
+            directories = [_bitPlatformProjectFolder];
+        }
+        if (filters == null || filters.Length == 0)
+        {
+            filters = ["*.*"];
+        }
+        var listFiles = await _searchService.SearchFilesAsync(directories, filters, true, searchText, matchCase, matchWholeWord);
+        if (listFiles.Count != 0)
+        {
+            await _searchService.ReplaceInFilesAsync(searchText, replaceText, listFiles, matchCase);
+        }
+        if (expectedReplacements != null && (expectedReplacements >= 0 ? listFiles.Count != expectedReplacements : listFiles.Count < -expectedReplacements))
+        {
+            _errors.Add($@"Replacements for ""{searchText}"" ({string.Join('|', filters)}): {listFiles.Count} / Expected: {(expectedReplacements >= 0 ? expectedReplacements : $">= {-expectedReplacements}")}");
+        }
+        return listFiles.Count;
+    }
+    private static bool RenameDirectory(string oldRelativePath, string newRelativePath)
+    {
+        var oldFullPath = Path.Combine(_bitPlatformProjectFolder, oldRelativePath);
+        var newFullPath = Path.Combine(_bitPlatformProjectFolder, newRelativePath);
+        if (Directory.Exists(newFullPath))
+        {
+            _errors.Add($@"The directory ""{newRelativePath}"" already exists.");
+            return true;
+        }
 
-        searchPattern = "Shared project to";
-        listFiles = await _searchService.SearchFilesAsync([_projectFolder], ["*.*"], true, searchPattern);
-        await _searchService.ReplaceInFilesAsync(searchPattern, $@"shared project (`{_projectName}.Core`) to", listFiles);
+        try
+        {
+            Directory.Move(oldFullPath, newFullPath);
+
+        }
+        catch (Exception)
+        {
+            _errors.Add($@"The directory ""{oldRelativePath}"" cannot be renamed.");
+            return false;
+        }
+        return true;
+    }
+    private static bool RenameFile(string oldRelativePath, string newRelativePath)
+    {
+        var oldFullPath = Path.Combine(_bitPlatformProjectFolder, oldRelativePath);
+        var newFullPath = Path.Combine(_bitPlatformProjectFolder, newRelativePath);
+        if (File.Exists(newFullPath))
+        {
+            _errors.Add($@"The file ""{newRelativePath}"" already exists.");
+            return true;
+        }
+        try
+        {
+            File.Move(oldFullPath, newFullPath);
+        }
+        catch (Exception)
+        {
+            _errors.Add($@"The file ""{oldRelativePath}"" cannot be renamed.");
+            return false;
+        }
+        return true;
     }
 }
