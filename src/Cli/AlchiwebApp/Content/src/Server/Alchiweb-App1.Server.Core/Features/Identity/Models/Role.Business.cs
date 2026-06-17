3940f57ecc5b1139988e4f141d61fddc70b51134
alchiweb <rev@ik.me>
2026-06-17 20:29:10 +0200

    feat: add custom models for User and Role

---
+++ src/Cli/AlchiwebApp/Content/src/Server/Alchiweb-App1.Server.Core/Features/Identity/Models/Role.Business.cs
@@ -0,0 +1,10 @@
+﻿namespace Alchiweb-App1.Server.Core.Features.Identity.Models;
+
+public partial class Role : IdentityRole<Guid>
+{
+    // [BusinessCustomCode]
+    //  Add custom constructors like:
+    //public Role() : base() { }
+    //public Role(string roleName) : base(roleName) { }
+}
+
---
+++ src/Cli/AlchiwebApp/Content/src/Server/Alchiweb-App1.Server.Core/Features/Identity/Models/User.Business.cs
@@ -0,0 +1,31 @@
+﻿// [BusinessCustomCode]
+//  Add usings like:
+//using Alchiweb-App1.Server.Domain.Users;
+//using Ardalis.Specification.EntityFrameworkCore.SharedKernel;
+
+namespace Alchiweb-App1.Server.Core.Features.Identity.Models;
+
+public partial class User : IdentityUser<Guid>
+    // [BusinessCustomCode]
+    //  Add interfaces like:
+    //, IEntityBase<User, Guid>, IApplicationUser
+{
+    // [BusinessCustomCode]
+    //  Add custom properties like:
+    //public DateTimeOffset CreatedAt { get; set; }
+    //public DateTimeOffset? LastConnection { get; set; }
+    //[PersonalData]
+    //public Guid? MemberId { get; set; }
+    //public RoleEnum Role
+    //{
+    //    get
+    //    {
+    //        try
+    //        {
+    //            return Enum.Parse<RoleEnum>(string.Join(',', Roles.Select(ur => ur.Role?.Name ?? "").ToArray()));
+    //        }
+    //        catch { return 0; }
+    //    }
+    //    set { }
+    //}
+}
