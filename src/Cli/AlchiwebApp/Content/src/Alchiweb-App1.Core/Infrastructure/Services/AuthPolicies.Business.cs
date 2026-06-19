8e65b6f0cf02d6eb80abf5161856e35a47ba7544
alchiweb <rev@ik.me>
2026-06-19 08:31:45 +0200

    fix: custom AuthPolicies

---
+++ src/Cli/AlchiwebApp/Content/src/Alchiweb-App1.Core/Infrastructure/Services/AuthPolicies.Business.cs
@@ -0,0 +1,8 @@
+﻿namespace Alchiweb-App1.Core.Infrastructure.Services;
+
+public partial class AuthPolicies
+{
+    // [BusinessCustomCode]
+    //  Add custom policy like:
+    //public const string USER_ACCESS = nameof(USER_ACCESS);
+}
