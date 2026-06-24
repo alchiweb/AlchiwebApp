a843f8e32f4227c97b0b7f8ebbeea640f93b6733
alchiweb <rev@ik.me>
2026-06-24 21:23:49 +0200

    feat: add custom services for Client.Core

---
+++ src/Cli/AlchiwebApp/Content/src/Client/Alchiweb-App1.Client.Core/Infrastructure/Extensions/IClientCoreServiceCollectionExtensions.Business.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Alchiweb-App1.Client.Core;
+using AlchiwebApp.Client.Core.AntD;
+using Alchiweb-App1.Client.Core.Infrastructure.Services.HttpMessageHandlers;
+using Alchiweb-App1.Core.Features.Security;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Services;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static partial class IClientCoreServiceCollectionExtensions
+{
+    public static IServiceCollection AddAlchiwebAppServices(this IServiceCollection services)
+    {
+        // For Ant Design Blazor
+        services.AddAntDesign();
+        services.AddSingleton<AntDComponentsConfiguration>(); // custom table localization for french language
+
+        // And Design Blazor toasted messages
+        services.AddScoped<IToastService, ToastService>();
+
+        services.AddScoped<IMediaService, MediaService>();
+        if (AppPlatform.IsBlazorHybridOrBrowser)
+        {
+            services.AddScoped<ICryptoService, CryptoService>();
+        }
+
+        return services;
+    }
+}
